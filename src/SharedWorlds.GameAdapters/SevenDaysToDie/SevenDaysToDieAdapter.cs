117636bd8effe2a2333f627d1c8efc7e6b65a511
KevinRabbe <rabbekevin@icloud.com>
2026-07-20 00:29:51 +0200

    Implement updated adapter contract for 7 Days to Die shell

--- src/SharedWorlds.GameAdapters/SevenDaysToDie/SevenDaysToDieAdapter.cs
+++ src/SharedWorlds.GameAdapters/SevenDaysToDie/SevenDaysToDieAdapter.cs
@@ -41,4 +41,10 @@ public sealed class SevenDaysToDieAdapter : IGameAdapter
 
     public Task WaitForSessionEndAsync(GameSessionHandle session, CancellationToken cancellationToken = default)
         => throw new NotImplementedException();
+
+    public Task FinalizePreparedWorldAsync(
+        PreparedWorld world,
+        PreparedWorldDisposition disposition,
+        CancellationToken cancellationToken = default)
+        => throw new NotImplementedException();
 }

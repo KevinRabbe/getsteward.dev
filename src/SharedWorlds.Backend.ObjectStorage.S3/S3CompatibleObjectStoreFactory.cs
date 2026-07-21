39462ce18f2f5885091ad49aa68e69bb063e4e9f
KevinRabbe <rabbekevin@icloud.com>
2026-07-21 23:03:57 +0200

    Create S3-compatible client from endpoint configuration

---
+++ src/SharedWorlds.Backend.ObjectStorage.S3/S3CompatibleObjectStoreFactory.cs
@@ -0,0 +1,22 @@
+using Amazon.Runtime;
+using Amazon.S3;
+
+namespace SharedWorlds.Backend.ObjectStorage.S3;
+
+public static class S3CompatibleObjectStoreFactory
+{
+    public static S3CompatibleImmutableObjectStore Create(S3CompatibleObjectStoreOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        var config = new AmazonS3Config
+        {
+            ServiceURL = options.ServiceUrl.AbsoluteUri.TrimEnd('/'),
+            AuthenticationRegion = options.AuthenticationRegion,
+            ForcePathStyle = options.ForcePathStyle
+        };
+        var credentials = new BasicAWSCredentials(options.AccessKeyId, options.SecretAccessKey);
+        var client = new AmazonS3Client(credentials, config);
+        return new S3CompatibleImmutableObjectStore(client, options.BucketName, ownsClient: true);
+    }
+}

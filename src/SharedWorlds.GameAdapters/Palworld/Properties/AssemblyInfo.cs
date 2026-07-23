185e8056cfa66854cbd63f9d20ebeff367113618
KevinRabbe <rabbekevin@icloud.com>
2026-07-23 02:46:36 +0200

    Allow Palworld WorldOption overlay probe internals access

--- src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
+++ src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SharedWorlds.GameAdapters.Palworld.Tests")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldProbe")]
+[assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionOverlayProbe")]

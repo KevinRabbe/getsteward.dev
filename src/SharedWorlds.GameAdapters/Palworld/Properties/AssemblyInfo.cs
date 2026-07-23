fc194105acac3ebeb2a1f832aec0ac65a3e87026
KevinRabbe <rabbekevin@icloud.com>
2026-07-23 15:11:11 +0200

    Expose Palworld adapter internals to PlM probe

--- src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
+++ src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("SharedWorlds.GameAdapters.Palworld.Tests")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldProbe")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionOverlayProbe")]
+[assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionPlMProbe")]

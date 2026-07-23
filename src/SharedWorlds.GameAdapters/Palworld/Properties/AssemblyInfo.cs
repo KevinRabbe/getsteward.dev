895e7e73f3f25cda31fc37e42f2126d963f3a01d
KevinRabbe <rabbekevin@icloud.com>
2026-07-23 23:53:27 +0200

    Allow Palworld runtime input lifecycle probe

--- src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
+++ src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
@@ -8,3 +8,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionSettingsProbe")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionIniMirrorProbe")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionArrayRestProbe")]
+[assembly: InternalsVisibleTo("SharedWorlds.PalworldRuntimeInputLifecycleProbe")]

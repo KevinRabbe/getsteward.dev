eedf385b4c5cd450b7e01f274bab734f56f44be9
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 20:16:11 +0200

    Prove Project Zomboid restore preflight cancellation

--- tests/SharedWorlds.GameAdapters.ProjectZomboid.Tests/ProjectZomboidStatePackagePreflightTests.cs
+++ tests/SharedWorlds.GameAdapters.ProjectZomboid.Tests/ProjectZomboidStatePackagePreflightTests.cs
@@ -42,6 +42,21 @@ public sealed class ProjectZomboidStatePackagePreflightTests : IDisposable
     }
 
     [Fact]
+    public void CancellationStopsMetadataScan()
+    {
+        var package = CreateArchive(("a.bin", new byte[] { 1 }));
+        using var archive = ZipFile.OpenRead(package);
+        using var cancellation = new CancellationTokenSource();
+        cancellation.Cancel();
+
+        Assert.Throws<OperationCanceledException>(() =>
+            ProjectZomboidStatePackagePreflight.GetDeclaredExtractionBytes(
+                archive,
+                maxEntries: 10,
+                cancellation.Token));
+    }
+
+    [Fact]
     public void FreeSpaceReserveFailsClosedWhenDeclaredExtractionCannotFit()
     {
         var exception = Assert.Throws<IOException>(() =>

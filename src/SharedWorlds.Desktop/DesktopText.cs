e76fd34b4122baeca677173afe8e3e9972c8f8f6
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 16:48:27 +0200

    Add Desktop resource string accessors

---
+++ src/SharedWorlds.Desktop/DesktopText.cs
@@ -0,0 +1,28 @@
+using System.Globalization;
+using System.Resources;
+
+namespace SharedWorlds.Desktop;
+
+public static class DesktopText
+{
+    private static readonly ResourceManager Resources = new(
+        "SharedWorlds.Desktop.DesktopText",
+        typeof(DesktopText).Assembly);
+
+    public static string Import => Get(nameof(Import));
+    public static string Refresh => Get(nameof(Refresh));
+    public static string BackToWorlds => Get(nameof(BackToWorlds));
+    public static string StartWorld => Get(nameof(StartWorld));
+    public static string HostWorld => Get(nameof(HostWorld));
+    public static string StopAndSave => Get(nameof(StopAndSave));
+    public static string ShareWorld => Get(nameof(ShareWorld));
+    public static string VerifyEnvironment => Get(nameof(VerifyEnvironment));
+    public static string Repair => Get(nameof(Repair));
+    public static string OpenSteward => Get(nameof(OpenSteward));
+    public static string QuitSteward => Get(nameof(QuitSteward));
+
+    private static string Get(string key)
+        => Resources.GetString(key, CultureInfo.CurrentUICulture)
+           ?? throw new MissingManifestResourceException(
+               $"Desktop string resource '{key}' is missing.");
+}

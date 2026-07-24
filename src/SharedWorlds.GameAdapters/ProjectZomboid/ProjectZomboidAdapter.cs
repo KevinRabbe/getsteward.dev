bbc2eb540749cff1a193c4fe85765b230e3634e9
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 14:04:02 +0200

    Delete unreachable Project Zomboid launch stubs

--- src/SharedWorlds.GameAdapters/ProjectZomboid/ProjectZomboidAdapter.cs
+++ src/SharedWorlds.GameAdapters/ProjectZomboid/ProjectZomboidAdapter.cs
@@ -77,18 +77,6 @@ public sealed class ProjectZomboidAdapter : IGameAdapter
         CancellationToken cancellationToken = default)
         => ProjectZomboidWorldState.RestorePreparedWorldAsync(world, state, cancellationToken);
 
-    public Task<GameSessionHandle> LaunchLocalAsync(PreparedWorld world, CancellationToken cancellationToken = default)
-        => throw new NotImplementedException();
-
-    public Task<GameSessionHandle> LaunchHostAsync(PreparedWorld world, CancellationToken cancellationToken = default)
-        => throw new NotImplementedException();
-
-    public Task<GameSessionHandle> LaunchClientAsync(PreparedWorld world, HostConnection host, CancellationToken cancellationToken = default)
-        => throw new NotImplementedException();
-
-    public Task WaitForSessionEndAsync(GameSessionHandle session, CancellationToken cancellationToken = default)
-        => throw new NotImplementedException();
-
     public Task FinalizePreparedWorldAsync(
         PreparedWorld world,
         PreparedWorldDisposition disposition,

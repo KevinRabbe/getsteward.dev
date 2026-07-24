5cdacad5b2596e44dca7c14eaf9dc1ba024f5c9d
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 03:05:32 +0200

    Keep only live Palworld friend assemblies

--- src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
+++ src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
@@ -2,8 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SharedWorlds.GameAdapters.Palworld.Tests")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldProbe")]
-[assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionStartupProbe")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionSettingsProbe")]
-[assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionIniMirrorProbe")]
-[assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionArrayRestProbe")]
-[assembly: InternalsVisibleTo("SharedWorlds.PalworldRuntimeInputLifecycleProbe")]

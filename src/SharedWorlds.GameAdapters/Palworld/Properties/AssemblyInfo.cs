eee69b2591dadbd90ed192d88235a292f4e392d8
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 02:43:24 +0200

    Remove obsolete Palworld probe friend assemblies

--- src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
+++ src/SharedWorlds.GameAdapters/Palworld/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SharedWorlds.GameAdapters.Palworld.Tests")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldProbe")]
-[assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionOverlayProbe")]
-[assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionPlMProbe")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionStartupProbe")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionSettingsProbe")]
 [assembly: InternalsVisibleTo("SharedWorlds.PalworldWorldOptionIniMirrorProbe")]

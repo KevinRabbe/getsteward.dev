a9af1f9dcb2c6cf71fa888d8460787c00cecdfcf
KevinRabbe <rabbekevin@icloud.com>
2026-07-27 17:55:33 +0200

    7DTD: friend lifecycle acceptance probe

--- src/SharedWorlds.GameAdapters/SevenDaysToDie/Properties/AssemblyInfo.cs
+++ src/SharedWorlds.GameAdapters/SevenDaysToDie/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SharedWorlds.GameAdapters.SevenDaysToDie.Tests")]
+[assembly: InternalsVisibleTo("SharedWorlds.SevenDaysToDieProbe")]

15b15c98198c7d04133b8f6d239038d85a62b77b
KevinRabbe <rabbekevin@icloud.com>
2026-08-04 08:46:05 +0200

    Persist compact restart-safe S3 transfer handles

--- src/SharedWorlds.Backend.ObjectStorage.S3/S3CompatibleObjectStoreFactory.cs
+++ src/SharedWorlds.Backend.ObjectStorage.S3/S3CompatibleObjectStoreFactory.cs
@@ -5,7 +5,8 @@ namespace SharedWorlds.Backend.ObjectStorage.S3;
 
 public static class S3CompatibleObjectStoreFactory
 {
-    public static RecoveringS3CompatibleImmutableObjectStore Create(S3CompatibleObjectStoreOptions options)
+    public static CompactS3CompatibleImmutableObjectStore Create(
+        S3CompatibleObjectStoreOptions options)
     {
         ArgumentNullException.ThrowIfNull(options);
 
@@ -20,10 +21,13 @@ public static class S3CompatibleObjectStoreFactory
         var protocol = options.ServiceUrl.Scheme == Uri.UriSchemeHttp
             ? Protocol.HTTP
             : Protocol.HTTPS;
-        return new RecoveringS3CompatibleImmutableObjectStore(
+        var recovering = new RecoveringS3CompatibleImmutableObjectStore(
             client,
             options.BucketName,
             protocol,
             ownsClient: true);
+        return new CompactS3CompatibleImmutableObjectStore(
+            recovering,
+            ownsInner: true);
     }
 }
